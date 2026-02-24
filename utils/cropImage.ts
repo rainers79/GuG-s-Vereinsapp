8ad7efbf5e1e86d9207a5d41801a8096cce136db
rainers79 <rainer@schmidt-kottingbrunn.at>
2026-02-24 18:32:41 +0100

    Create cropImage.ts

---
+++ utils/cropImage.ts
@@ -0,0 +1,38 @@
+// utils/cropImage.ts
+
+export const getCroppedImg = async (
+  imageSrc: string,
+  croppedAreaPixels: any
+): Promise<Blob> => {
+
+  const image = new Image();
+  image.src = imageSrc;
+
+  await new Promise((resolve) => {
+    image.onload = resolve;
+  });
+
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = croppedAreaPixels.width;
+  canvas.height = croppedAreaPixels.height;
+
+  ctx?.drawImage(
+    image,
+    croppedAreaPixels.x,
+    croppedAreaPixels.y,
+    croppedAreaPixels.width,
+    croppedAreaPixels.height,
+    0,
+    0,
+    croppedAreaPixels.width,
+    croppedAreaPixels.height
+  );
+
+  return new Promise((resolve) => {
+    canvas.toBlob((blob) => {
+      resolve(blob as Blob);
+    }, 'image/jpeg', 0.9);
+  });
+};
